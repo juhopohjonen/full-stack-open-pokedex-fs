2bea97d789bcfbc1dd6d25caf26fb8c3bbdca69a
Juho <juhopohjonen237@gmail.com>
2024-08-21 21:21:18 +0300

    adjust fly.toml

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-const goodHealth = false;
+const goodHealth = true;
 
 app.get('/health', (req, res) => {
   if (goodHealth) {
