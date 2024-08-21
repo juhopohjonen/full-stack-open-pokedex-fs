810c1f135b1bfc7564515dfb5844d1cd4ad290b4
Juho <juhopohjonen237@gmail.com>
2024-08-21 20:56:05 +0300

    add health api endpoint

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+const goodHealth = true;
+
+app.get('/health', (req, res) => {
+  if (goodHealth) {
+    return res.status(200).send('ok')
+  }
+
+  return res.status(500).send('fail')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
