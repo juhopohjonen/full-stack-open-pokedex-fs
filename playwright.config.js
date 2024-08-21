9ba71734fafd1059660c31e974c9cac333bc01e6
Juho <juhopohjonen237@gmail.com>
2024-08-21 12:05:49 +0300

    add playwright & tests & include it to the workflow

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './e2etests',
+  testDir: './e2e-tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -69,11 +69,15 @@ module.exports = defineConfig({
     // },
   ],
 
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    reuseExistingServer: false,
+  },
+
+
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  
+
 });
 
