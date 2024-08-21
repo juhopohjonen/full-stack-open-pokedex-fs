bc2f392cf0395d9129c0be765ab16117f63f0ea9
Juho <juhopohjonen237@gmail.com>
2024-08-21 16:21:01 +0300

    testing health api endpoint

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get('/health', (req, res) => {
+  const fail = process.env.HEALTH == 'fail'
+
+  return res.status(fail ? 500 : 200).send(fail ? 'fail' : 'ok')
+
+})
+
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
