c54be7e029d1c68c4cd3eeca918c1d56293bed80
Juho <juhopohjonen237@gmail.com>
2024-08-21 19:16:03 +0300

    revert on where fly deploy works

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ const app = express();
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.static("dist"));
-
-app.get('/health', (req, res) => {
-  const fail = process.env.HEALTH === 'fail'
-
-  return res.status(fail ? 500 : 200).send(fail ? 'fail' : 'ok')
-
-})
-
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
